c4f03a1b988dbaad65651edf0d5a6907fc189702
Alex Kozin <al@el-machine.com>
2025-08-08 19:22:07 +0700

    Add RestModelFormatted

---
+++ Sources/WandURL/REST/dto/RestModelFormatted.swift
@@ -0,0 +1,44 @@
+///
+/// Copyright © 2020-2024 El Machine 🤖
+/// https://el-machine.com/
+///
+/// Licensed under the Apache License, Version 2.0 (the "License");
+/// you may not use this file except in compliance with the License.
+/// You may obtain a copy of the License at
+///
+/// 1) LICENSE file
+/// 2) https://apache.org/licenses/LICENSE-2.0
+///
+/// Unless required by applicable law or agreed to in writing, software
+/// distributed under the License is distributed on an "AS IS" BASIS,
+/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+/// See the License for the specific language governing permissions and
+/// limitations under the License.
+///
+/// Created by Alex Kozin
+/// 2020 El Machine
+
+#if canImport(Foundation)
+import Foundation.NSURL
+import Wand
+
+public
+protocol RestModelFormatted: Rest.Model {
+
+}
+
+@discardableResult
+@inline(__always)
+public
+func | <T: RestModelFormatted> (id: Int, get: Ask<T>.Get) -> Core {
+    Core(String(format: T.path, id)| as URL) | get
+}
+
+//TODO:
+//extension Rest.Model {
+//
+//    typealias Formatted = RestModelFormatted
+//
+//}
+
+#endif
